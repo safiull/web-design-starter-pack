99af24cc758c682751e0d0892226aab106f11f4f
safiull <safiullalam9931@gmail.com>
2021-05-04 23:31:29 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+    	// Slicknav
+		$('#main-menu').slicknav({
+	        closeOnClick: true,
+	        label: '',
+	        appendTo:'.mobile-menu',
+	    });
+        
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
